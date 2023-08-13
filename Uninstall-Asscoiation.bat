38b921713c6055b4415e97d891cec50e61df8600
TBG09 <105569316+TBG09@users.noreply.github.com>
2023-08-12 23:33:16 -0400

    Update Uninstall-Asscoiation.bat

--- Uninstall-Asscoiation.bat
+++ Uninstall-Asscoiation.bat
@@ -1,22 +1,17 @@
 @echo off
 setlocal
 
+:: Specify the extension and registry key path
 set "extension=.ncms"
-set "exePath=F:\nircmd-x64\nircmd-CLI.exe"
+set "registryKey=HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\FileExts\.ncms"
 
-:: Remove file extension association
-reg delete HKCU\Software\Classes\%extension% /f
-reg delete HKCU\Software\Classes\.%extension% /f
+:: Delete the registry key for the file extension association
+reg delete "%registryKey%" /f
 
-:: Update system file association cache
+:: Remove the association from the system file association cache
 assoc %extension%=
 
-:: Remove association from file types
-for /f "delims=" %%i in ('ftype %extension%') do (
-    set "ftypeLine=%%i"
-    if not "!ftypeLine:%exePath%=!"=="!ftypeLine!" (
-        ftype %extension%=
-    )
-)
+:: Remove the application as the default handler for the file type
+ftype %extension%=
 
 endlocal
